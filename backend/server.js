00c47c94481e7e1144e40e508d8c5fcdca61ee5d
shakedshoshan <shakedshoshan8@gmail.com>
2024-09-27 19:21:37 +0300

    deploy

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,10 @@ import { Server } from 'socket.io';
 import path from "path";
 import { fileURLToPath } from 'url';
 
+
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); 
+const __dirname = path.join(path.dirname(__filename), '..', '..', 'frontend');
+console.log(__dirname);
 
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
