422771c3010ceea214f38ebfb76170efd6ea0b1f
shakedshoshan <shakedshoshan8@gmail.com>
2024-09-27 17:13:34 +0300

    deploy

--- backend/server.js
+++ backend/server.js
@@ -70,11 +70,9 @@ io.on('connection', (socket) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, "./frontend/dist")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "frontend/dist/index.html")));
 
 server.listen(PORT, () => {
     connectToMongoDB();
